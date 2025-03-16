d302db926096e6b75f93bd62889a29a05feb32f4
Berkay Cengiz <berkaycengiz12321@gmail.com>
2025-03-16 18:53:40 +0300

    fix

--- pages/api/todo/index.tsx
+++ pages/api/todo/index.tsx
@@ -1,8 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import {getAllData} from "@/services/serviceOperations";
 
-
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if(req.method === 'GET'){
         try {
             const data = await getAllData("listElements");
