24da4a5a326596f9d797d08af58aa5af8cd76d71
Berkay Cengiz <berkaycengiz12321@gmail.com>
2025-03-16 20:01:17 +0300

    cors fix for vercel deployment.

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  async headers() {
-    return [
-      {
-        source: "pages/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
